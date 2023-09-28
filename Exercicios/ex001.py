87851d304e1da84e288d14fa38e183245fd069a1
João Victor W. Klein <joaovictorklein555@gmail.com>
2023-09-28 17:32:13 -0300

    Create ex001.py

---
+++ Exercicios/ex001.py
@@ -0,0 +1,11 @@
+print('Olá Mundo')
+print(7+4)
+print('7' + '4')
+nome='Klein'
+idade='17'
+peso='70.0'
+print(nome,idade,peso)
+nome2=input('Qual e o seu nome?')
+idade2=input('Qual a sua idade?')
+peso2=input('Qual o seu peso?')
+print(nome2,idade2,peso2)
