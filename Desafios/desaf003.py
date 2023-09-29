f3a28a230114862c674fe4095229ad59ab6e0dca
João Victor W. Klein <joaovictorklein555@gmail.com>
2023-09-29 17:26:28 -0300

    estudos

--- Desafios/desaf003.py
+++ Desafios/desaf003.py
@@ -1,5 +1,6 @@
 print("Informe dois numeros")
-numero1=input("numero1=")
-numero2=input("numero2=")
+numero1=int (input("numero1="))
+numero2=int (input("numero2="))
 soma= numero1+numero2
-print("A soma dos dois numeros e:", soma)
+#print("A soma entre",numero1 ,"e",numero2,"é: {}".format(soma))
+print('A soma entra {} e {} equivale a {}' .format(numero1, numero2, soma))
